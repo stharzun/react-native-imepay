94d3ae5fc6eed5ae41b6d36a8d1b73c70f175bf5
Arjun Shrestha <stharzun@gmail.com>
2023-02-23 21:37:52 +0545

    updated version android' gi

--- android/src/main/java/com/reactnativeimepay/ImepayModule.java
+++ android/src/main/java/com/reactnativeimepay/ImepayModule.java
@@ -17,55 +17,56 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+
 import java.util.Map;
 
 public class ImepayModule extends ReactContextBaseJavaModule {
   ImepayModule(ReactApplicationContext context) {
-       super(context);
-   }
+    super(context);
+  }
 
-    @Override
-    public String getName() {
-        return "Imepay";
-    }
+  @Override
+  public String getName() {
+    return "Imepay";
+  }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    public void makePayment(String MERCHANT_CODE, String MERCHANT_NAME,String MERCAHNT_TRANSACTION_RECORDING_URL,String AMOUNT,String REFERENCE_ID,String MODULE,String USERNAME,String PASSWORD, String ENV,  Promise promise) {
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void makePayment(String MERCHANT_CODE, String MERCHANT_NAME, String MERCHANT_TRANSACTION_RECORDING_URL, String AMOUNT, String REFERENCE_ID, String MODULE, String USERNAME, String PASSWORD, String ENV, Promise promise) {
 
 
-        IMEPayment imePayment = new IMEPayment(getCurrentActivity(), ENV.equals("PROD") ? ENVIRONMENT.LIVE: ENVIRONMENT.TEST);
+    IMEPayment imePayment = new IMEPayment(getCurrentActivity(), ENV.equals("PROD") ? ENVIRONMENT.LIVE : ENVIRONMENT.TEST);
 
-        imePayment.performPayment(MERCHANT_CODE,
-                          MERCHANT_NAME,
-                          MERCAHNT_TRANSACTION_RECORDING_URL,
-                          AMOUNT,
-                          REFERENCE_ID,
-                          MODULE,
-                          USERNAME,
-                          PASSWORD,
-                          new IMEPaymentCallback() {
-           @Override
-           public void onSuccess(int responseCode, String responseDescription, String transactionId, String msisdn, String    amount, String refId) {
-              // Response Code 100 : Transaction successful.
-              // Response Code 101 : Transaction failed.
-              // responseDescription : Message sent from server, contains transaction success message/ failure message with reason
-              // transactionId : Unique ID generated from IME pay system
-              // msisdn : Customer Mobile Number
-              // amount : Amount paid by customer
-              // refId : Reference Value
-             WritableMap map = Arguments.createMap();
-             map.putString("responseCode", String.valueOf(responseCode));
-             map.putString("responseDescription", responseDescription);
-             map.putString("transactionId", transactionId);
-             map.putString("msisdn", msisdn);
-             map.putString("amount", amount);
-             map.putString("refId", refId);
-              promise.resolve(map);
-           }
-});
-    }
+    imePayment.performPaymentV1(MERCHANT_CODE,
+      MERCHANT_NAME,
+      MERCHANT_TRANSACTION_RECORDING_URL,
+      AMOUNT,
+      REFERENCE_ID,
+      MODULE,
+      USERNAME,
+      PASSWORD,
+      new IMEPaymentCallback() {
+        @Override
+        public void onSuccess(int responseCode, String responseDescription, String transactionId, String msisdn, String amount, String refId) {
+          // Response Code 100 : Transaction successful.
+          // Response Code 101 : Transaction failed.
+          // responseDescription : Message sent from server, contains transaction success message/ failure message with reason
+          // transactionId : Unique ID generated from IME pay system
+          // msisdn : Customer Mobile Number
+          // amount : Amount paid by customer
+          // refId : Reference Value
+          WritableMap map = Arguments.createMap();
+          map.putString("responseCode", String.valueOf(responseCode));
+          map.putString("responseDescription", responseDescription);
+          map.putString("transactionId", transactionId);
+          map.putString("msisdn", msisdn);
+          map.putString("amount", amount);
+          map.putString("refId", refId);
+          promise.resolve(map);
+        }
+      });
+  }
 
 
 }
